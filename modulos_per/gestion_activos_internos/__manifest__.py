8b73f8decbf96109cea54fb4949b11b54e6f0098
akthaly <bryan2014ap@gmail.com>
2026-02-13 20:24:48 -0600

    commit: estructura inicial de proyecto, configuración de Docker, y .gitignore

---
+++ modulos_per/gestion_activos_internos/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Gestión de Activos Internos",
+    'version': '1.0',
+    'category': 'Recursos Humanos',
+    'summary': 'Este módulo esta diseñado para manejar el control y gestión de activos internos de la empresa',
+    'description': """
+        Con este módulo podremos registrar, controlar y dar seguimiento a los 
+        bienes físicos y digitales que forman parte de su operación diaria. """,
+    'author': "Alejandro Anona",
+    'depends': ['base', 'hr'],
+    'data': [
+        # rutas a los archivos XML de vistas, acciones, menús, etc.
+    ]
+    'installable': True,
+    'application': True,
+
+}
